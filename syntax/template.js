db15c1a35ad27e5d11ff5e1ff28c72dfe1da6972
JayHan88 <53374442+JayHan88@users.noreply.github.com>
2019-08-05 01:28:09 +0900

    8/5 01:00 home

---
+++ syntax/template.js
@@ -0,0 +1,10 @@
+// Template Literal
+
+var name = 'Han';
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'+name+' incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute '+name+'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Jay Excepteur sint occaecat cupidatat Jay non proident, sunt in culpa qui officia deserunt mollit '+name+' anim id est laborum.';
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ${name} incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ${name} irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Jay Excepteur sint occaecat cupidatat Jay non proident, sunt in culpa qui officia deserunt mollit ${name} anim id est laborum. ${1+1}`;
+
+console.log(letter);
---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'Han';
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'+name+' incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute '+name+'irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Jay Excepteur sint occaecat cupidatat Jay non proident, sunt in culpa qui officia deserunt mollit '+name+' anim id est laborum.';
+console.log(letter);
